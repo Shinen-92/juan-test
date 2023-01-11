93e450d1be72f8c327403c2f6837cf0c161b3f33
Shinen <shinen92@gmail.com>
2023-01-11 22:37:56 +0100

    Juan

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,40 @@
+import { useEffect } from "react";
 import Test from "./components/Test";
+import { juanes, juanImages } from './data/juanes'
 
 const App = () => {
+  
+  useEffect(() => {
+    const url = new URL(window.location.href)
+    const param = url.searchParams.get('juan') || ''
+    const juan = juanes.find(juan => param === juan.name)
+    
+    if (!juan) {
+      return
+    }
+
+    document.title = `Test de personaliJuan - Soy ${juan.name}`
+    const titles = [
+      document.head.children.namedItem('twitter:title'),
+      document.head.querySelector('[property="og:title"]')
+    ]
+    titles.forEach(d => d.content = document.title)
+    const descriptions = [
+      document.head.children.namedItem('description'),
+      document.head.children.namedItem('twitter:description'),
+      document.head.querySelector('[property="og:description"]')
+    ]
+    descriptions.forEach(d => d.content = juan.description)
+    
+    const images = [
+      document.head.children.namedItem('twitter:image'),
+      document.head.querySelector('[property="og:image"]')
+    ]
+    images.forEach(d => d.content = juanImages(`./${juan.image}`))
+    
+
+  }, [])
+
   return (
     <div className="bg-rose-800 h-full flex items-center justify-center"><Test /></div>
   )
