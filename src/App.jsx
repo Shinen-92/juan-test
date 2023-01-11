6951488410e8d990abf5c971a021cc1417854def
Shinen <shinen92@gmail.com>
2023-01-11 17:53:13 +0100

    Add tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
-const App = () => <div>Juan in progress...</div>
+const App = () => {
+  return (
+    <div className="bg-rose-600 text-white h-full flex items-center justify-center text-8xl font-bold">Juan in progress...</div>
+  )
+}
 
 export default App;
